b2716d4290ee6d11de04aa5557c68492af675209
christikaes <christikaes@gmail.com>
2017-07-20 12:51:49 -0400

    AngularMaterial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { MusicalBoxComponent } from './musical-box/musical-box.component';
@@ -19,7 +21,9 @@ import { AuthenticationComponent } from './authentication/authentication.compone
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [
     AuthenticationService,

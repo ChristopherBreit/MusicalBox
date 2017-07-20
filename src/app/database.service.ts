2c74aca331650c82f17e020ea540f784ca8e7b42
christikaes <christikaes@gmail.com>
2017-07-20 18:35:36 -0400

    AngularFireDatabase

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -1,24 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class DatabaseService {
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afDatabase: AngularFireDatabase
+  ) { }
 
   // Returns an observable stream of an array of the current user's musical boxes
   public $myMusicalBoxList() {
-    // TODO!!
-    return new Observable((observer)=> {
-      observer.next([])
+    return new Observable((observer) => {
+      this.afAuth.authState.subscribe((user) => {
+        if (user) {
+          const uid = user.uid;
+          this.afDatabase.list('players/' + uid).subscribe(boxList => {
+            observer.next(boxList);
+          })
+        } else {
+          observer.next([])
+        }
+      })
+      observer.next([]);
     })
   }
 
-  // Returns an observable stream of an array of all the public musical boxes
+  // Returns an observablestream of an array of all the public musical boxes
   public $publicMusicalBoxList() {
-    // TODO!!
-    return new Observable((observer)=> {
-      observer.next([])
+    return new Observable((observer) => {
+      this.afDatabase.list('players/').subscribe(players => {
+        const boxList = [];
+        players.forEach(player => {
+          Object.keys(player).map(key => player[key]).forEach( box => {
+            boxList.push(box);
+          })
+        })
+        observer.next(boxList);
+      })
+      observer.next([]);
     })
   }
 
